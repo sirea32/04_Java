3291c6820d60e1ca40f3e0900d779facabfe7b06
sirea32 <sirea32@gmail.com>
2024-07-19 16:51:49 +0900

    20240719 끌라스 필드 객체배열 학습

---
+++ 05_oop/src/constructor/MemberRun.java
@@ -0,0 +1,46 @@
+package constructor;
+
+public class MemberRun {
+
+	public static void main(String[] args) {
+		
+		// Member 객체 생성 1 : 생성후 setter로 값 초기화
+		Member mem1 = new Member();
+					   // Member() 기본 생성자
+					   // -> Member 클래스에 작성되어 있음
+		
+		
+		// mem1이 참조하는 객체의 setMemberId() 메서드 호출
+		mem1.setMemberId("member01");
+		mem1.setMemberPw("pass01");
+		mem1.setMemberName("김희원");
+		mem1.setMemberAge(27);
+		
+		mem1.inform(); // mem1이 참조하는 객체의 필드 값 출력
+		
+		
+		// ----------------------------------------------------
+		
+		// Member 객체 생성 2 : 매개 변수 생성자 이용
+		
+		Member mem2 = new Member("member02", "pass02", "최회원", 22);
+		// -> 객체가 만들어지면서 동시에
+		//	  생성자에 의해서 필드값이 초기화 된다!!!
+		
+		// setter 방법 5줄 -> 생성자 방법 1줄
+		
+		mem2.inform();
+		
+		// ** 매개 변수 생성자로 얻은 효과 **
+		// -> 코드 길이 감소 (== 재사용성 증가)
+		
+		
+		// --------------------------------------------------
+		
+		// this() 생산자 확인
+		Member mem3 = new Member();
+		mem3.inform();
+		
+		
+	}
+}
