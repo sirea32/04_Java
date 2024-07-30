672bb0a2d1895921f3518f70a7d060b36aa4e261
sirea32 <sirea32@gmail.com>
2024-07-30 17:02:18 +0900

    20240730 컬렉션 마무리 예외 학습 시작

---
+++ 10_exception/src/pkg2/run/TryCatchFinallyRun.java
@@ -0,0 +1,18 @@
+package pkg2.run;
+
+import pkg2.service.TryCatchFinallyService;
+
+public class TryCatchFinallyRun {
+	public static void main(String[] args) {
+	
+		TryCatchFinallyService service = new TryCatchFinallyService();
+		
+		
+//		service.test1();
+		service.test2();
+//		service.test3();
+//		service.test4();
+//		service.test5();
+//		service.test6();		
+	}
+}
